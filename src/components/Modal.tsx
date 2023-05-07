8a88eb896e8010f90a9f4bd86b0acb649ee5ff67
joncoder <joncoder.kamite@gmail.com>
2023-05-07 12:30:39 +0200

    modal REACT component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Modal.css';
+
+type Props = {
+  message: string;
+  onClick(): void;
+}
+
+const Modal = ({message, onClick}: Props) => {
+  return (
+    <div className="modal">
+      <div className="modal-contents">
+        <p>{message}</p>
+        <button
+          onClick={onClick}
+        >Play again</button>
+      </div>
+    </div>
+  )
+}
+
+export default Modal
